5edff47648c18767fdb33917650ed2c87a76e276
wangjq <wangjq729@126.com>
2019-10-25 01:04:10 +0800

    fix

--- jq-test-utils/src/main/java/com/jq/test/testng/DingTalkListener.java
+++ jq-test-utils/src/main/java/com/jq/test/testng/DingTalkListener.java
@@ -1,12 +1,11 @@
 package com.jq.test.testng;
 
-import com.dingtalk.api.DefaultDingTalkClient;
-import com.dingtalk.api.DingTalkClient;
-import com.dingtalk.api.request.OapiRobotSendRequest;
-import com.dingtalk.api.response.OapiRobotSendResponse;
 import com.jq.test.task.ITestMethod;
-import com.taobao.api.ApiException;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestTemplate;
 import org.testng.*;
 
 import java.text.SimpleDateFormat;
@@ -14,17 +13,24 @@ import java.util.Date;
 import java.util.Map;
 
 public class DingTalkListener implements ISuiteListener {
-    private DingTalkClient client = new DefaultDingTalkClient("https://oapi.dingtalk.com/robot/send?access_token=aa9a76956e0bcf39e7d5ddccc5437fc9afd11e81fecb41569d4b5415dd95c8e9");
+    private String url = "https://oapi.dingtalk.com/robot/send?access_token=aa9a76956e0bcf39e7d5ddccc5437fc9afd11e81fecb41569d4b5415dd95c8e9";
     private String platform = System.getProperty("platform");
     private String features = System.getProperty("features");
     private String test_feature = System.getProperty("test.feature");
 
     @Override
     public void onStart(ISuite suite) {
-        OapiRobotSendRequest request = new OapiRobotSendRequest();
-        request.setMsgtype("markdown");
-        OapiRobotSendRequest.Markdown markdown = new OapiRobotSendRequest.Markdown();
-        markdown.setTitle("脚本开始运行");
+        String body = "{\n" +
+                "     \"msgtype\": \"markdown\",\n" +
+                "     \"markdown\": {\n" +
+                "         \"title\":\"脚本开始运行\",\n" +
+                "         \"text\": \"%s\"\n" +
+                "     },\n" +
+                "    \"at\": {\n" +
+                "        \"atMobiles\": [], \n" +
+                "        \"isAtAll\": false\n" +
+                "    }\n" +
+                " }";
         StringBuilder builder = new StringBuilder();
         builder.append("开始运行自动化测试：\n");
         builder.append(String.format("> #### 平台：%s\n", platform));
@@ -34,21 +40,23 @@ public class DingTalkListener implements ISuiteListener {
         }
         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         builder.append("> ###### 开始时间： ").append(df.format(new Date())).append("\n");
-        markdown.setText(builder.toString());
-        request.setMarkdown(markdown);
-        try {
-            OapiRobotSendResponse response = client.execute(request);
-        } catch (ApiException e) {
-            e.printStackTrace();
-        }
+        doPost(body, builder);
+
     }
 
     @Override
     public void onFinish(ISuite suite) {
-        OapiRobotSendRequest request = new OapiRobotSendRequest();
-        request.setMsgtype("markdown");
-        OapiRobotSendRequest.Markdown markdown = new OapiRobotSendRequest.Markdown();
-        markdown.setTitle("脚本测试结果");
+        String body = "{\n" +
+                "     \"msgtype\": \"markdown\",\n" +
+                "     \"markdown\": {\n" +
+                "         \"title\":\"脚本测试结果\",\n" +
+                "         \"text\": \"%s\"\n" +
+                "     },\n" +
+                "    \"at\": {\n" +
+                "        \"atMobiles\": [], \n" +
+                "        \"isAtAll\": false\n" +
+                "    }\n" +
+                " }";
         StringBuilder builder = new StringBuilder();
         builder.append("测试结果：\n");
         builder.append(String.format("> #### 平台：%s\n", platform));
@@ -73,13 +81,15 @@ public class DingTalkListener implements ISuiteListener {
         }
         builder.append("> ###### [点击查看测试报告](http://10.0.0.152:8080/job/java-interface-test/allure/) \n");
         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        builder.append("> ###### 结束时间： ").append(df.format(new Date())).append("\n");
-        markdown.setText(builder.toString());
-        request.setMarkdown(markdown);
-        try {
-            OapiRobotSendResponse response = client.execute(request);
-        } catch (ApiException e) {
-            e.printStackTrace();
-        }
+        doPost(body, builder);
+    }
+
+    private void doPost(String body, StringBuilder builder) {
+        RestTemplate restTemplate = new RestTemplate();
+        String format = String.format(body, builder.toString());
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+        HttpEntity entity = new HttpEntity<>(format, headers);
+        String result = restTemplate.postForObject(url, entity, String.class);
     }
 }
