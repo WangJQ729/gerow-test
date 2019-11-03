7389e0571b142a3fe770dc0e3dd86f872d748cc1
wangjq <wangjq729@126.com>
2019-11-03 11:56:50 +0800

    直接调用服务，不走test tools

--- jq-test-utils/src/main/java/com/jq/test/testng/CaseListListener.java
+++ jq-test-utils/src/main/java/com/jq/test/testng/CaseListListener.java
@@ -3,6 +3,8 @@ package com.jq.test.testng;
 import com.alibaba.fastjson.JSONObject;
 import com.jq.test.task.ITestMethod;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -15,6 +17,7 @@ public class CaseListListener implements ITestListener {
 
 
     private static String token;
+    private final static Log logger = LogFactory.getLog(CaseListListener.class);
 
     @Override
     public void onTestStart(ITestResult result) {
@@ -34,24 +37,28 @@ public class CaseListListener implements ITestListener {
         caseInfo.setDescription(description);
         caseInfo.setCreator(creator);
         caseInfo.setSeverity(severityLevel);
-        doPost(caseInfo);
+        try {
+            doPost(caseInfo);
+        } catch (Exception e) {
+            logger.warn("添加case失败！");
+        }
     }
 
     private void doPost(CaseInfo caseInfo) {
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
-        String host = "http://test-tools.xiaoduoai.com";
+        String host = "http://zhy-test1.xiaoduoai.com";
         if (StringUtils.isEmpty(token)) {
             HttpEntity entity = new HttpEntity<>("{\"username\":\"wangjianqiang\",\"password\":\"wangjianqiang123456\"}\n", headers);
-            String login = "/test1/v1/login/";
+            String login = "/api/v1/login/";
             JSONObject loginResult = restTemplate.postForObject(host + login, entity, JSONObject.class);
             assert loginResult != null;
             token = loginResult.getJSONObject("data").getString("token");
         }
         headers.set(HttpHeaders.AUTHORIZATION, "auth " + token);
         HttpEntity entity = new HttpEntity<>(caseInfo, headers);
-        String caseCreate = "/test1/v1/auto_test/caseCreate";
+        String caseCreate = "/api/v1/auto_test/caseCreate";
         restTemplate.postForObject(host + caseCreate, entity, JSONObject.class);
     }
 
