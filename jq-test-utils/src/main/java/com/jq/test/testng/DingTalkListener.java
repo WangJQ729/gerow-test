f8d0993c72f4370ccf7be96f834a56c7f0e6e8be
wangjq <wangjq729@126.com>
2019-12-19 15:00:33 +0800

    rename DingTalkListener method

--- jq-test-utils/src/main/java/com/jq/test/testng/DingTalkListener.java
+++ jq-test-utils/src/main/java/com/jq/test/testng/DingTalkListener.java
@@ -36,7 +36,7 @@ public class DingTalkListener implements ISuiteListener {
                 "        \"isAtAll\": false\n" +
                 "    }\n" +
                 " }";
-        StringBuilder builder = builderMessageBody("脚本开始运行：\n");
+        StringBuilder builder = builderMessage("脚本开始运行：\n");
         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         builder.append("> ###### 开始时间： ").append(df.format(new Date())).append("\n");
         doPost(body, builder);
@@ -55,58 +55,13 @@ public class DingTalkListener implements ISuiteListener {
                 "        \"isAtAll\": false\n" +
                 "    }\n" +
                 " }";
-        StringBuilder builder = builderMessageBody("脚本运行结果：\n");
-        builder.append(buildMessageResult(suite));
+        StringBuilder builder = builderMessage("脚本运行结果：\n");
+        builder.append(buildResultMessage(suite));
         builder.append(String.format("> ###### [点击查看测试报告](%s) \n", System.getProperty("build_url") + "allure/"));
         doPost(body, builder);
     }
 
-    private StringBuilder buildMessageResult(ISuite suite) {
-        StringBuilder builder = new StringBuilder();
-        Map<String, ISuiteResult> suiteResults = suite.getResults();
-        for (ISuiteResult result : suiteResults.values()) {
-            ITestContext tc = result.getTestContext();
-            int pass = tc.getPassedTests().getAllResults().size();
-            builder.append("> ###### total Passed: ").append(pass).append("\n");
-            Set<ITestResult> failedResult = tc.getFailedTests().getAllResults();
-            int failed = failedResult.size();
-            builder.append("> ###### total Failed: ").append(failed).append("\n");
-            Set<ITestResult> skippedResult = tc.getSkippedTests().getAllResults();
-            int skipped = skippedResult.size();
-            builder.append("> ###### total Skipped: ").append(skipped).append("\n");
-            builder.append(buildTimeCost(tc.getEndDate().toInstant().getEpochSecond() - tc.getStartDate().toInstant().getEpochSecond()));
-            if (failed > 0) {
-                builder.append("> ###### Failed list:\n");
-                for (ITestResult allResult : failedResult) {
-                    String failedName = ((ITestMethod) allResult.getParameters()[0]).getTestClass().getStory() + "-" + allResult.getName();
-                    builder.append("> ######    ").append(failedName).append("\n");
-                }
-            }
-            if (skipped > 0) {
-                builder.append("> ###### Skipped list:\n");
-                for (ITestResult allResult : skippedResult) {
-                    String skippedName = ((ITestMethod) allResult.getParameters()[0]).getTestClass().getStory() + "-" + allResult.getName();
-                    builder.append("> ######    ").append(skippedName).append("\n");
-                }
-            }
-        }
-        return builder;
-    }
-
-    private StringBuilder buildTimeCost(long compare) {
-        StringBuilder builder = new StringBuilder();
-        builder.append("> ###### 耗时: ");
-        if (compare / 60 > 60) {
-            builder.append(compare / 60 / 60).append("小时");
-            builder.append(compare / 60 % 60).append("分");
-        } else {
-            builder.append(compare / 60).append("分");
-        }
-        builder.append(compare % 60).append("秒").append("\n");
-        return builder;
-    }
-
-    private StringBuilder builderMessageBody(String title) {
+    private StringBuilder builderMessage(String title) {
         StringBuilder builder = new StringBuilder();
         Properties properties = new Properties();
         String file = String.format("application-%s.yml", profiles);
@@ -129,8 +84,53 @@ public class DingTalkListener implements ISuiteListener {
         return builder;
     }
 
+    private StringBuilder buildResultMessage(ISuite suite) {
+        StringBuilder builder = new StringBuilder();
+        Map<String, ISuiteResult> suiteResults = suite.getResults();
+        for (ISuiteResult result : suiteResults.values()) {
+            ITestContext tc = result.getTestContext();
+            int pass = tc.getPassedTests().getAllResults().size();
+            builder.append("> ###### total Passed: ").append(pass).append("\n");
+            Set<ITestResult> failedResult = tc.getFailedTests().getAllResults();
+            int failed = failedResult.size();
+            builder.append("> ###### total Failed: ").append(failed).append("\n");
+            Set<ITestResult> skippedResult = tc.getSkippedTests().getAllResults();
+            int skipped = skippedResult.size();
+            builder.append("> ###### total Skipped: ").append(skipped).append("\n");
+            builder.append(buildUseTime(tc.getEndDate().toInstant().getEpochSecond() - tc.getStartDate().toInstant().getEpochSecond()));
+            if (failed > 0) {
+                builder.append("> ###### Failed list:\n");
+                for (ITestResult allResult : failedResult) {
+                    String failedName = ((ITestMethod) allResult.getParameters()[0]).getTestClass().getStory() + "-" + allResult.getName();
+                    builder.append("> ######    ").append(failedName).append("\n");
+                }
+            }
+            if (skipped > 0) {
+                builder.append("> ###### Skipped list:\n");
+                for (ITestResult allResult : skippedResult) {
+                    String skippedName = ((ITestMethod) allResult.getParameters()[0]).getTestClass().getStory() + "-" + allResult.getName();
+                    builder.append("> ######    ").append(skippedName).append("\n");
+                }
+            }
+        }
+        return builder;
+    }
+
+    private StringBuilder buildUseTime(long compare) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("> ###### 耗时: ");
+        if (compare / 60 > 60) {
+            builder.append(compare / 60 / 60).append("小时");
+            builder.append(compare / 60 % 60).append("分");
+        } else {
+            builder.append(compare / 60).append("分");
+        }
+        builder.append(compare % 60).append("秒").append("\n");
+        return builder;
+    }
+
     private void doPost(String body, StringBuilder builder) {
-        if (isSendMessage()) {
+        if (isSendMessageTime()) {
             RestTemplate restTemplate = new RestTemplate();
             String format = String.format(body, builder.toString());
             HttpHeaders headers = new HttpHeaders();
@@ -142,7 +142,7 @@ public class DingTalkListener implements ISuiteListener {
         }
     }
 
-    private boolean isSendMessage() {
+    private boolean isSendMessageTime() {
         int hour = LocalTime.now().getHour();
         return hour >= 9 && hour <= 22;
     }
